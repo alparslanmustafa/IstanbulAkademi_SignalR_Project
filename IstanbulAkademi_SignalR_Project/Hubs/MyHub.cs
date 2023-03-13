129cd503d56ad6812c831d392c633c97aca92e89
alpar <alpar@DESKTOP-VG05C7A>
2023-03-13 12:23:24 +0300

    Add project files.

---
+++ IstanbulAkademi_SignalR_Project/Hubs/MyHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IstanbulAkademi_SignalR_Project.Hubs
+{
+    public class MyHub:Hub
+    {
+        public static List<string> Names { get; set; }
+        public async Task SendName(string name)
+        {
+            Names.Add(name);
+            await Clients.All.SendAsync("ReceiveName",name);
+        }
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync("ReceiveNames",Names);
+        }
+    }
+}
